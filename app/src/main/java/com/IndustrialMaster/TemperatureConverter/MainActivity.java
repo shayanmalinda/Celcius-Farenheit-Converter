462c0ae85f7b45231a48f64d7a4de65453e96c2c
shayanmalinda <shayanmalinda@gmail.com>
2019-02-20 20:06:28 +0530

    Logic for convert celcius into farenheit

--- app/src/main/java/com/IndustrialMaster/TemperatureConverter/MainActivity.java
+++ app/src/main/java/com/IndustrialMaster/TemperatureConverter/MainActivity.java
@@ -2,6 +2,8 @@ package com.IndustrialMaster.TemperatureConverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convert(View v){
+        EditText et1 = findViewById(R.id.editText);
+        EditText et2 = findViewById(R.id.editText2);
+        String text = et1.getText().toString();
+        double celcius = Double.parseDouble(text);
+        double farenheit = ((celcius*(9.0/5.0))+32.0);
+        et2.setText(String.valueOf(farenheit));
+
     }
 }
